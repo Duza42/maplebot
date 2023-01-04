649541eb88d783d18e90ec5761b575705df18f34
Duza42 <Duza42@gmail.com>
2023-01-04 13:45:04 -0600

    Add job notificaitons and column to /rank

--- maplebot/maplebot.py
+++ maplebot/maplebot.py
@@ -20,6 +20,91 @@ SAMPLES_PATH = "samples/"
 MAIN_CONFIG_FILE = "config.yaml"
 LOGGING_CONFIG_FILE = "logging.yaml"
 
+JOBS = {
+    0: 'Beginner',
+    100: 'Warrior',
+    110: 'Fighter',
+    111: 'Crusader',
+    112: 'Hero',
+    120: 'Page',
+    121: 'White Knight',
+    122: 'Paladin',
+    130: 'Spearman',
+    131: 'Dragon Knight',
+    132: 'Dark Knight',
+    200: 'Magician',
+    210: 'Fire/Poison Wizard',
+    211: 'Fire/Poison Mage',
+    212: 'Fire/Poison ArchMage',
+    220: 'Ice/Lightning Wizard',
+    221: 'Ice/Lightning Mage',
+    222: 'Ice/Lightning ArchMage',
+    230: 'Cleric',
+    231: 'Priest',
+    232: 'Bishop',
+    300: 'Bowman',
+    310: 'Hunter',
+    311: 'Ranger',
+    312: 'Bowmaster',
+    320: 'Crossbowman',
+    321: 'Sniper',
+    322: 'Marksman',
+    400: 'Thief',
+    410: 'Assassin',
+    411: 'Hermit',
+    412: 'Night Lord',
+    420: 'Bandit',
+    421: 'Chief Bandit',
+    422: 'Shadower',
+    500: 'Pirate',
+    510: 'Brawler',
+    511: 'Marauder',
+    512: 'Buccaneer',
+    520: 'Gunslinger',
+    521: 'Outlaw',
+    522: 'Corsair',
+    800: 'Maple Leaf Brigadier',
+    900: 'GM',
+    910: 'Super GM',
+    1000: 'Noblesse',
+    1100: 'Dawn Warrior 1',
+    1110: 'Dawn Warrior 2',
+    1111: 'Dawn Warrior 3',
+    1112: 'Dawn Warrior 4',
+    1200: 'Blaze Wizard 1',
+    1210: 'Blaze Wizard 2',
+    1211: 'Blaze Wizard 3',
+    1212: 'Blaze Wizard 4',
+    1300: 'Wind Archer 1',
+    1310: 'Wind Archer 2',
+    1311: 'Wind Archer 3',
+    1312: 'Wind Archer 4',
+    1400: 'Night Walker 1',
+    1410: 'Night Walker 2',
+    1411: 'Night Walker 3',
+    1412: 'Night Walker 4',
+    1500: 'Thunder Breaker 1',
+    1510: 'Thunder Breaker 2',
+    1511: 'Thunder Breaker 3',
+    1512: 'Thunder Breaker 4',
+    2000: 'Legend',
+    2001: 'Evan',
+    2100: 'Aran 1',
+    2110: 'Aran 2',
+    2111: 'Aran 3',
+    2112: 'Aran 4',
+    2200: 'Evan 1',
+    2210: 'Evan 2',
+    2211: 'Evan 3',
+    2212: 'Evan 4',
+    2213: 'Evan 5',
+    2214: 'Evan 6',
+    2215: 'Evan 7',
+    2216: 'Evan 8',
+    2217: 'Evan 9',
+    2218: 'Evan 10'
+}
+
 
 # Read data from YAML file
 def read_yaml_file(filename):
@@ -110,10 +195,10 @@ class MapleBot(discord.Client):
         LOGGER.info('Attempting fetch player data')
         try:
             cur = self.cnx.cursor()
-            cur.execute("SELECT name, level FROM characters WHERE name != 'Admin' ORDER BY level DESC;")
+            cur.execute("SELECT name, level, job FROM characters WHERE name != 'Admin' ORDER BY level DESC;")
             LOGGER.info('Successfully fetched player data')
             for player in cur:
-                self.players[player[0]] = Player(player[0], player[1])
+                self.players[player[0]] = Player(player[0], player[1], JOBS[player[2]])
             cur.close()
             if len(self.previousPlayers) != 0:
                 for player in self.players.values():
@@ -132,10 +217,10 @@ class MapleBot(discord.Client):
                                     LOGGER.info('error getting previous player', str(e))
                             await channel.send(level_message)
                             LOGGER.info(level_message)
-                        # if previous_player.job != player.job:
-                        #    job_message = f'{player.name} is now a {player.job}!'
-                        #    await channel.send(job_message)
-                        #    LOGGER.info(job_message)
+                        if previous_player.job != player.job:
+                            job_message = f'{player.name} is now a {player.job}!'
+                            await channel.send(job_message)
+                            LOGGER.info(job_message)
                     except KeyError as e:
                         LOGGER.info('old_player does not exist, recently added?', str(e))
 
@@ -167,9 +252,10 @@ class PlayerNotfications(commands.Cog):
 
 
 class Player(object):
-    def __init__(self, name, level):
+    def __init__(self, name, level, job):
         self.name = name
         self.level = level
+        self.job = job
 
 
 init_logging()
@@ -188,10 +274,10 @@ async def _ping(ctx):  # Defines a new "context" (ctx) command called "ping."
 @tree.command(name="rank", description="Lists the rank for all characters", guild=discord.Object(id=guild_id))
 async def _rank(ctx):  # Defines a new "context" (ctx) command called "ping."
     table = texttable.Texttable()
-    table.set_cols_align(["l", "l"])
-    table.add_row(["Name", "Level"])
+    table.set_cols_align(["l", "l", "l"])
+    table.add_row(["Name", "Level", "Job"])
     for player in client.previousPlayers.values():
-        table.add_row([player.name, player.level])
+        table.add_row([player.name, player.level, player.job])
     await ctx.response.send_message("```\n" + table.draw() + "\n```\n")
 
 
